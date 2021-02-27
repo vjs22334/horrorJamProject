cb4a1453c6ef9e620ebad1e1cd3cc74b5f72bfa1
vjs22334 <vjs122334@gmail.com>
2021-02-27 22:04:43 +0530

    added basic player controller,camera, input system

---
+++ Assets/InputSystem/InputActions.cs
@@ -0,0 +1,205 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/InputSystem/InputActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputActions"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""fefcec7a-38bc-4226-abb3-5663b81fa944"",
+            ""actions"": [
+                {
+                    ""name"": ""movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""4ea1b98e-6a4f-484f-974b-fd66ed90427c"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Sneak"",
+                    ""type"": ""Button"",
+                    ""id"": ""def1c9f4-9c50-4d1b-907a-3617394c569f"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""83ebcc16-fbd0-4ac0-a97a-5cde657cf074"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""fb9684fb-85f7-466e-9f7d-7f4d5b782f73"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""80f0b843-eaa5-4ee0-821f-ecbad24487ae"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""ef073df0-5d86-47f7-ab26-98b1aa228718"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""c6a35ec7-97a7-43cf-af07-f69982138cc4"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""8e8b2e8a-f71c-4b69-92bb-e07bece5146e"",
+                    ""path"": ""<Keyboard>/ctrl"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Sneak"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_movement = m_Player.FindAction("movement", throwIfNotFound: true);
+        m_Player_Sneak = m_Player.FindAction("Sneak", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_movement;
+    private readonly InputAction m_Player_Sneak;
+    public struct PlayerActions
+    {
+        private @InputActions m_Wrapper;
+        public PlayerActions(@InputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @movement => m_Wrapper.m_Player_movement;
+        public InputAction @Sneak => m_Wrapper.m_Player_Sneak;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Sneak.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSneak;
+                @Sneak.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSneak;
+                @Sneak.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSneak;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @movement.started += instance.OnMovement;
+                @movement.performed += instance.OnMovement;
+                @movement.canceled += instance.OnMovement;
+                @Sneak.started += instance.OnSneak;
+                @Sneak.performed += instance.OnSneak;
+                @Sneak.canceled += instance.OnSneak;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnSneak(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("movement data")]
+    public float normalMovementSpeed = 10; // normal movement, noisy
+    public float quietMovementSpeed = 2.5f; // for sneaking
+
+    [Header("References")]
+    InputActions inputActions;
+    Rigidbody2D rigidbody;
+    Animator animator;
+
+    Vector2 moveDir = Vector2.zero;
+    float moveSpeed;
+
+    #region Monobehaviour
+
+        void Awake()
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
+            inputActions = new InputActions();
+            moveSpeed = normalMovementSpeed;            
+        }
+        void OnEnable()
+        {
+            inputActions.Player.movement.performed += PlayerMovementHandler;
+            inputActions.Player.movement.canceled += PlayerMovementStoppedHandler;
+            inputActions.Player.Sneak.performed += SneakHandler;
+            inputActions.Player.Sneak.canceled += SneakCanceledHandler;
+            inputActions.Player.Enable();
+        }
+
+
+        void OnDisable()
+        {
+            inputActions.Player.movement.performed -= PlayerMovementHandler;
+            inputActions.Player.movement.canceled -= PlayerMovementStoppedHandler;
+            inputActions.Player.Sneak.performed -= SneakHandler;
+            inputActions.Player.Sneak.canceled -= SneakCanceledHandler;
+            inputActions.Player.Disable();
+        }
+
+        void Update()
+        {
+            HandleAnimations();
+        }
+
+    
+        void FixedUpdate()
+        {
+            rigidbody.velocity = moveDir*moveSpeed;
+        }
+    #endregion
+
+    #region InputHandlers
+        private void PlayerMovementHandler(InputAction.CallbackContext context)
+        {
+            moveDir = context.ReadValue<Vector2>().normalized;
+        }
+
+        private void PlayerMovementStoppedHandler(InputAction.CallbackContext context)
+        {
+            moveDir = Vector2.zero;
+        }
+
+        private void SneakCanceledHandler(InputAction.CallbackContext context)
+        {
+            moveSpeed = normalMovementSpeed;
+        }
+
+        private void SneakHandler(InputAction.CallbackContext context)
+        {
+            moveSpeed = quietMovementSpeed;
+        }
+
+    #endregion
+
+    #region Animation
+        void HandleAnimations(){
+            if(moveDir.magnitude!=0){
+                animator.SetFloat("moveX",moveDir.x);
+                animator.SetFloat("moveY",moveDir.y);
+            }
+            animator.SetFloat("speed",moveSpeed*moveDir.magnitude);
+        }
+    #endregion
+}
